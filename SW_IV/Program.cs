5323981f43acfb4388f43216bfb38e8c99e6273d
RemiBardon <37386490+RemiBardon@users.noreply.github.com>
2018-03-25 20:49:17 +0200

    Add files via upload

---
+++ SW_IV/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SW_IV {
+    class Program {
+        static void Main(string[] args) {
+            int x = 930;
+            int y = 321;
+            int z = 967;
+
+            while (10 * x > y) {
+                x = (y * z) % 10000;
+                y = (3 * z) % 10000;
+                z = (7 * z) % 10000;
+            }
+
+            Console.WriteLine("{0}, {1}, {2}", x, y, z);
+            Console.ReadLine();
+        }
+    }
+}
